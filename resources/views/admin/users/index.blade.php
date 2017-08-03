3c7569a25b30dc6b2266f601c5a479361ffd9d96
Ivan Santos <ivanildo.silva@fapema.br>
2017-08-03 18:16:10 -0300

    Trabalhando com ícones

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -13,8 +13,8 @@
              ->callback('Ações', function ($field, $model){
                 $linkEdit = route('admin.users.edit', ['user' => $model->id]);
                 $linkShow = route('admin.users.show', ['user' => $model->id]);
-                return Button::link('Editar')->asLinkTo($linkEdit).'|'.
-                    Button::link('Ver')->asLinkTo($linkShow);
+                return Button::link(Icon::create('pencil').' Editar')->asLinkTo($linkEdit).'|'.
+                    Button::link(Icon::create('folder-open').'&nbsp;&nbsp;Ver')->asLinkTo($linkShow);
              })!!}
         </div>
         {!! $users->links() !!}
