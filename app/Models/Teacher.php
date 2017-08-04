7405fca439977d15a014176804454d7e03230f85
Ivan Santos <ivanildo.silva@fapema.br>
2017-08-04 14:17:11 -0300

    v

---
+++ app/Models/Teacher.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Educ\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Teacher extends Model
+{
+    public function user()
+    {
+        return $this->morphOne(User::class, 'userable');
+    }
+}
